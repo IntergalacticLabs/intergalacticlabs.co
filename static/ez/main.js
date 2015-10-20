28f1b049fc019b03d6330ad96f830a48c792e995
Peter Brandt <peter.m.brandt@gmail.com>
2015-10-19 23:41:37 -0400

    adding some concept of editing

--- static/ez/main.js
+++ static/ez/main.js
@@ -10,17 +10,24 @@ var map = L.mapbox.map('map', 'mapbox.mars-satellite')
 // });
 // viking232m.addTo(map);
 
+function log() {
+  console.log.apply(console, arguments);
+}
+
+log.error = function() {
+  console.log.apply(console, arguments);
+}
+log.err = log.error;
+
 
 var featureGroup = L.featureGroup().addTo(map);
 
 // Define circle options
 // http://leafletjs.com/reference.html#circle
 var circle_options = {
-    color: '#fff',      // Stroke color
-    opacity: 1,         // Stroke opacity
-    weight: 10,         // Stroke weight
-    fillColor: '#000',  // Fill color
-    fillOpacity: 0.6    // Fill opacity
+    color: '#dcb439',      // Stroke color
+    fillColor: '#dcb439',  // Fill color
+    fillOpacity: 0.25    // Fill opacity
 };
 
 // Define polyline options
@@ -33,11 +40,22 @@ var polyline_options = {
 var drawControl = new L.Control.Draw({
   edit: {
     featureGroup: featureGroup
+  }, draw: {
+    circle: {
+      shapeOptions: circle_options
+    }
   }
 }).addTo(map);
 
 map.on('draw:created', function(e) {
-    featureGroup.addLayer(e.layer);
+  var id = e.id = Math.random()*10000000|0;
+  e.layer.on('click', function() {
+    log('clicked', id);
+    edit(e);
+  });
+  featureGroup.addLayer(e.layer);
+  log('created', id);
+  edit(e);
 });
 
 var coordinates = $('#coordinates');
@@ -48,3 +66,28 @@ map.on('move', function(e) {
 
 var ll = map.getCenter();
 coordinates.text('Lon: ' + ll.lng.toPrecision(5) + ', Lat: ' + ll.lat.toPrecision(5) + ', z: ' + map.getZoom());
+
+var currentEditingNode = null;
+
+function exitEditMode() {
+  if (currentEditingNode) {
+    currentEditingNode.layer.editing.disable();
+  }
+  currentEditingNode = null;
+}
+
+function edit(e) {
+  if (currentEditingNode === e) {
+    log('already editing', e.id);
+    return;
+  }
+  exitEditMode();
+  currentEditingNode = e;
+  e.layer.editing.enable();
+  log('editing', e);
+}
+
+map.on('click', function() {
+  log('clicked map');
+  exitEditMode();
+})
